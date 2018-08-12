b1a09d949d3ab50cfe5fe2bfa37d7404cd7803e1
frodo <mrunderhill.bree@gmail.com>
2018-08-12 08:46:23 -0700

    changed defaultUser to just be an id

--- server/boot/01-user.js
+++ server/boot/01-user.js
@@ -34,24 +34,61 @@ module.exports = (server) => {
   });
 
   const node = server.models.Node;
+  const vNode = server.models.VNode;
   const nodeRevision = server.models.NodeRevision;
   const edge = server.models.Edge;
   const edgeRevision = server.models.EdgeRevision;
+  const createNodeRevision = async ({result}) => {
+    // Will I get the entire object back, or do I have to re-query it?
+    const revisionResult = await server.models.NodeRevision.create({nodeId: result.id});
+    const revision = await server.models.NodeRevision.findById(revisionResult.id);
+
+    result.nodeRevisions = [revision];
+  };
   const addUser = async (ctx) => {
     ctx.req.body.userId = ctx.req.accessToken.userId;
   };
   node.beforeRemote('create', addUser);
+  node.afterRemote('create', createNodeRevision);
   nodeRevision.beforeRemote('create', addUser);
   edge.beforeRemote('create', addUser);
   edgeRevision.beforeRemote('create', addUser);
 
-  user.hasMany(node, {});
-  user.hasMany(edge, {});
   defaultUser.hasMany(node, {
-    foreignKey: "userId"
+    foreignKey: 'userId',
+  });
+  defaultUser.hasMany(vNode, {
+    foreignKey: 'userId',
   });
   defaultUser.hasMany(edge, {
-    foreignKey: "userId"
+    foreignKey: 'userId',
   });
+  user.hasMany(node, {});
+  user.hasMany(vNode, {});
+  user.hasMany(edge, {});
+  /*  // TODO How do I create this ACL?  Doesn't the model have an acl property?
+    debugger;*/
+// Do I need to make sure that the user is creating an node which belongs to it?
+  user.settings.acls.push(
+    {
+      "principalType": "ROLE",
+      "principalId": "$authenticated",
+      "permission": "ALLOW",
+      "property": "__create__nodes",
+    },
+    {
+      "principalType": "ROLE",
+      "principalId": "$authenticated",
+      "permission": "ALLOW",
+      "property": "",
+    }
+  );
   user.emit('remoteMethodAdded');
+
+  user.nestRemoting('nodes');
+  user.nestRemoting('vNodes');
+  user.nestRemoting('edges');
+  node.nestRemoting('nodeRevisions');
+  vNode.nestRemoting('nodeRevisions');
+  edge.nestRemoting('edgeRevisions');
 };
