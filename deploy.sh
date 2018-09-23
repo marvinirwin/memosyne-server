520434472b1a93508ec39cfce2553e796027eb1f
frodo <mrunderhill.bree@gmail.com>
2018-09-23 14:08:36 -0700

    still need to create relation between vNode and vEdge

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-# cp -r server/*.json ./dist/server/;
-# cp -r common dist/common;
-# cp -r client dist/client;
-# cp -r node_modules dist/node_modules;
-# cp datasources.local.js dist/server/datasources.local.js;
-# cp package.json dist/package.json;
-# cp -r ./.elasticbeanstalk ./dist/.elasticbeanstalk;
-
-# cd dist;
-
 cp datasources.local.js server/datasources.local.js;
 zip memosyne-server -r * .[^.]*;
 rm server/datasources.local.js;
