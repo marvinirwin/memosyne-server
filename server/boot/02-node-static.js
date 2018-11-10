3e2e5963766b06101fe74dee8ab1d4842cb581d0
frodo <mrunderhill.bree@gmail.com>
2018-11-10 12:57:39 -0800

    Added the static method, it's doesn't work correctly, but it gets all the information to work correctly

--- server/boot/02-node-static.js
+++ server/boot/02-node-static.js
@@ -1,35 +1,117 @@
 const path = require('path');
-const dots = require("dot").process({path: path.resolve(__dirname, '../views')});
+const dot = require("dot");
+
+const _ = require('lodash');
+const showdown = require('showdown');
+const converter = new showdown.Converter();
+converter.setOption('tables', true);
+const siblingQuery = `
+    SELECT sibling.*
+    FROM v_edge parent_to_child
+    JOIN v_node parent ON (parent_to_child.n1 = parent.id)
+    JOIN v_edge parent_to_siblings ON (parent.id = parent_to_siblings.n1)
+    JOIN v_node sibling ON (sibling.id = parent_to_siblings.n2)
+    WHERE parent_to_child.n2 = $1;
+`;
+const parentQuery = `
+    SELECT parent.*
+    FROM v_edge parent_to_child
+    JOIN v_node parent ON (parent_to_child.n1 = parent.id)
+    WHERE parent_to_child.n2 = $1;
+`;
+const childQuery = `
+    SELECT  child.*
+    FROM v_edge parent_to_child
+    JOIN v_node child ON (parent_to_child.n2 = child.id)
+    WHERE parent_to_child.n1 = $1;
+`;
+const rootNodesQuery = `
+SELECT n.*
+FROM v_nested_sets_graph g 
+JOIN v_node n ON (g.node_id = n.id)
+WHERE g.user_id = $1 AND g.lft = 1;
+`;
+
+function camelCaseObject(o) {
+  Object.keys(o).map(k => {
+    const camel = _.camelCase(k);
+    if (camel !== k) {
+      o[camel] = o[k];
+      delete o[k];
+    }
+  });
+  return o;
+}
+
+function generateMarkdown(o) {
+  o.markdown = converter.makeHtml(o.text);
+  o.text = o.text || '';
+  o.sanitizedText = o.text
+    .replace('#', '')
+    .replace('*', '');
+  o.summary = o.sanitizedText.split('\n')[0];
+  o.markdownSummary = converter.makeHtml(o.summary);
+  return o
+}
+
+function exec(datasource, query, params) {
+  return new Promise((resolve, reject) => {
+    try {
+      datasource.connector.execute(query, params, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.map(camelCaseObject).map(generateMarkdown));
+        }
+      })
+    } catch (e) {
+      throw e;
+    }
+  });
+}
 
 module.exports = (server) => {
-  return;
   // How do I make it so that you can only call this with /api/nodes/{number}?
   // anyways let's just assume we have permission to see everybody
-  const graph = server.models.VNestedSetsGraph;
-  const user = server.models.User;
-  if (!graph) {
-    throw 'Graph undefined in node-static';
+  const vGraphModel = server.models.VNestedSetsGraph;
+  const vNodeModel = server.models.VNode;
+  const datasource = server.datasources.aws;
+  if (![vGraphModel, vNodeModel, datasource].every(v => v)) {
+    throw 'Not everything found undefined in node-static';
   }
 
-  graph.static = async (id) => {
-    // first select all graph roots which belong to that user with their nodes
+  vGraphModel.static = async (id) => {
+    if (!id) {
+      const r = await vGraphModel.find({where: {text: {neq: ''}, lft: 1}, limit: 1});
+      id = r[0].id;
+    }
+    // First select all graph roots which belong to that user with their nodes
     // We'll use that for the side bar
-    const rootVNodes = await user.__nestedSetsGraphs__VNodes(id);
-    // Now grab all that graph's vNode
-    const parent = await graph.__VNode(id);
-    // Now grab all that node's immediate children
-    // Maybe have to make a relation VNode -> OutoingVEdge -> IncomingVEdge.  Maybe we need their children?
-    const immediateChildren = await graph.__VNode__OutgoingVEdges__VNode(id);
+    try {
+      const graph = await vGraphModel.findById(id);
+      const node = await vNodeModel.findById(graph.nodeId);
+      // Graphs may not have a userId, I may have to use vGraph
+      const userRootNodes = await exec(datasource, rootNodesQuery, [graph.userId]);
+      const siblingNodes = await exec(datasource, siblingQuery, [node.id]);
+      const parentNodes = await exec(datasource, parentQuery, [node.id]);
+      const childNodes = await exec(datasource, childQuery, [node.id]);
 
+      let dots = dot.process({path: path.resolve(__dirname, '../views')});
+      const result = dots.node({
+        node,
+        userRootNodes,
+        siblingNodes,
+        parentNodes,
+        childNodes
+      });
+      return result;
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  // So to load a node's static we need to query all that node's nestedSset
-  nodes.static = (id) => {
-
-  };
-  app.get('/static', async function(req, res){
-    // TODO figure out how to render my dots template into a request
-    const result = dots.node({parent:{text:'icecream'}});
-    console.log(result);
+  server.get('/static', async function (req, res) {
+    const response = await vGraphModel.static(req.params.graphId);
+    res.send(response);
   });
 };
